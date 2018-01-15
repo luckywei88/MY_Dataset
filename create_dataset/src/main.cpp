65ca25946f749e6d79bf1fff8791a3cd2bee5afe
luckywei88 <1010224593@qq.com>
2018-01-15 11:23:48 +0800

    first commit

---
+++ create_dataset/src/main.cpp
@@ -0,0 +1,95 @@
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+using namespace std;
+using namespace cv;
+
+string dir;
+fstream depio,rgbio;
+
+void callback(const sensor_msgs::ImageConstPtr& rgb, const sensor_msgs::ImageConstPtr& dep)
+{
+    ros::Time rgb_t=rgb->header.stamp;
+    ros::Time dep_t=dep->header.stamp;
+    cv_bridge::CvImagePtr rgb_cv,dep_cv;
+    cv::Mat rgb_m,dep_m;
+    try
+    {
+	rgb_cv=cv_bridge::toCvCopy(rgb,sensor_msgs::image_encodings::BGR8);
+	rgb_m=rgb_cv->image;
+	dep_cv=cv_bridge::toCvCopy(dep,"16UC1");
+	dep_m=dep_cv->image;
+    }
+    catch(cv_bridge::Exception& e)
+    {
+	ROS_ERROR("cv_bridge exception: %s", e.what());
+    }
+    string rgbbuf,depbuf;
+    stringstream rgbs; 
+    stringstream deps;
+    rgbs<<dir<<"/rgb/"<<rgb_t<<".png";
+    deps<<dir<<"/depth/"<<dep_t<<".png";
+    rgbs>>rgbbuf;
+    deps>>depbuf;
+    imshow("rgb",rgb_m);
+    cvWaitKey(20);
+	rgbio<<rgb_t<<" "<<"rgb/"<<rgb_t<<".png"<<endl;
+	depio<<dep_t<<" "<<"depth/"<<dep_t<<".png"<<endl;
+	imwrite(rgbbuf,rgb_m);
+	/*
+	IplImage* depthImage=new IplImage(dep_m);
+	IplImage* img = cvCreateImage(cvSize(depthImage->width, depthImage->height), IPL_DEPTH_8U, 1);
+	char* depthData = (char*)depthImage->imageData;
+	for (int i = 0, j = 0; i < depthImage->imageSize; i+=2, ++j)
+	        img->imageData[j] = (((depthData[i + 1] << 5) + (depthData[i] >> 3)) / 4096.0);
+		*/
+	imwrite(depbuf,dep_m);
+}
+
+int main(int argc,char **argv)
+{
+    ROS_ERROR("start");
+    string dataset="lucky";
+    dir="/home/lucky/dataset/";
+    ros::init(argc,argv,"dataset");
+    ros::start();
+    ros::NodeHandle nh;
+    ros::NodeHandle n_private("~");
+    n_private.param("dataset",dataset,dataset);
+    dir=dir+dataset;
+    string rgbdir,depdir;
+    rgbdir=dir+"/rgb";
+    depdir=dir+"/depth";
+    mkdir(dir.c_str(),S_IRUSR | S_IWUSR | S_IXUSR | S_IRWXG | S_IRWXO);
+    mkdir(rgbdir.c_str(),S_IRUSR | S_IWUSR | S_IXUSR | S_IRWXG | S_IRWXO);
+    mkdir(depdir.c_str(),S_IRUSR | S_IWUSR | S_IXUSR | S_IRWXG | S_IRWXO);
+    string rgbfile,depfile,groundfile;
+    rgbfile=dir+"/rgb.txt";
+    depfile=dir+"/depth.txt";
+    rgbio.open(rgbfile.c_str(),ofstream::out|ofstream::trunc);
+    depio.open(depfile.c_str(),ofstream::out|ofstream::trunc);
+    message_filters::Subscriber<sensor_msgs::Image> rgb_sub(nh,"/rgb",2);
+    message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh,"/depth",2);
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
+    message_filters::Synchronizer<sync_pol> sync(sync_pol(10), rgb_sub,depth_sub);
+    sync.registerCallback(boost::bind(callback,_1,_2));
+    ros::spin();
+    rgbio.close();
+    depio.close();
+    ROS_ERROR("close");
+    ros::shutdown();
+    return 0;
+}
---
+++ send_dataset/src/main.cpp
@@ -0,0 +1,156 @@
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <chrono>
+
+using namespace std;
+using namespace cv;
+
+ros::Publisher rgbpub,deppub;
+
+void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageFilenamesRGB,
+		vector<string> &vstrImageFilenamesD, vector<double> &vTimestamps)
+{
+	ifstream fAssociation;
+	fAssociation.open(strAssociationFilename.c_str());
+	while(!fAssociation.eof())
+	{
+		string s;
+		getline(fAssociation,s);
+		if(!s.empty())
+		{
+			stringstream ss;
+			ss << s;
+			double t;
+			string sRGB, sD;
+			ss >> t;
+			vTimestamps.push_back(t);
+			ss >> sRGB;
+			vstrImageFilenamesRGB.push_back(sRGB);
+			ss >> t;
+			ss >> sD;
+			vstrImageFilenamesD.push_back(sD);
+
+		}
+	}
+}
+
+void Send_Image(cv::Mat &rgbm, cv::Mat &depm, double &timestamps)
+{
+	cv_bridge::CvImage rgb,dep;
+	sensor_msgs::Image rgb_image,dep_image;
+
+	rgb.encoding=sensor_msgs::image_encodings::BGR8;
+	rgb.image=rgbm;
+	rgb.toImageMsg(rgb_image);
+
+	dep.encoding="16UC1";
+	dep.image=depm;
+	dep.toImageMsg(dep_image);
+
+	rgbpub.publish(rgb_image);
+	deppub.publish(dep_image);
+}
+
+int main(int argc,char **argv)
+{
+	string sequence="lucky";
+	string dataset="lucky";
+	string dir="/home/lucky/dataset/";
+
+	ros::init(argc,argv,"send_dataset");
+	ros::start();
+	ros::NodeHandle nh;
+	ros::NodeHandle n_private("~");
+	n_private.param("dataset",dataset,dataset);
+	n_private.param("sequence",sequence,sequence);
+	rgbpub=nh.advertise<sensor_msgs::Image>("/rgb",1);
+	deppub=nh.advertise<sensor_msgs::Image>("/depth",1);
+	
+	dir=dir+dataset+"/"+sequence;
+	string rgbdir,depdir;
+	rgbdir=dir+"/rgb";
+	depdir=dir+"/depth";
+	string assofile;
+	assofile=dir+"/associate.txt";
+
+	vector<string> vstrImageFilenamesRGB;
+	vector<string> vstrImageFilenamesD;
+	vector<double> vTimestamps; 
+	LoadImages(assofile, vstrImageFilenamesRGB, vstrImageFilenamesD, vTimestamps);
+
+	
+	int nImages = vstrImageFilenamesRGB.size();
+	if(vstrImageFilenamesRGB.empty())
+	{
+		cerr << endl << "No images found in provided path." << endl;
+		return 1;
+	}
+	else if(vstrImageFilenamesD.size()!=vstrImageFilenamesRGB.size())
+	{
+		cerr << endl << "Different number of images for rgb and depth." << endl;
+		return 1;
+	}
+
+	cv::Mat imRGB, imD;
+	for(int ni=0; ni<nImages; ni++)
+	{
+		// Read image and depthmap from file
+		imRGB = cv::imread(dir+"/"+vstrImageFilenamesRGB[ni],CV_LOAD_IMAGE_UNCHANGED);
+		imD = cv::imread(dir+"/"+vstrImageFilenamesD[ni],CV_LOAD_IMAGE_UNCHANGED);
+		double tframe = vTimestamps[ni];
+
+		if(imRGB.empty())
+		{
+			cerr << endl << "Failed to load image at: "
+				<< dir << "/" << vstrImageFilenamesRGB[ni] << endl;
+			return 1;
+		}
+
+#ifdef COMPILEDWITHC11
+		std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+		std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+		// send to ROS
+		Send_Image(imRGB,imD,tframe);
+
+#ifdef COMPILEDWITHC11
+		std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+		std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+
+		double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+		// Wait to load the next frame
+		double T=0;
+		if(ni<nImages-1)
+			T = vTimestamps[ni+1]-tframe;
+		else if(ni>0)
+			T = tframe-vTimestamps[ni-1];
+
+		if(ttrack<T)
+			usleep((T-ttrack)*1e6);
+		
+		ros::spinOnce();
+		ROS_ERROR("send");
+	}
+
+
+	ros::shutdown();
+	return 0;
+}
